62c5488526b6fae9cfa3b8adb794ac1aa31d57e4
hranikBs23 <Habibur.Rahman@brainstation-23.com>
2023-03-23 15:20:54 +0600

    Storyboard Db setup

---
+++ Notes/Notes.API/Models/Entities/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.API.Models.Entities
+{
+    public class Note
+    {
+        public Guid id { get; set; }
+        public string Ttile { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible {get; set; }
+    }
+}
